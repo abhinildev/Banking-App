42713002de72c69da349640d7a1f1a3b327de0d9
abhinildev <savarniabhinil@gmail.com>
2025-07-28 17:39:36 +0530

    Deploying 3

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -3,7 +3,7 @@ import {createClient} from "redis"
 const redisClient= createClient({
     socket:{
         host: process.env.REDIS_HOST,
-        port: parseInt(process.env.REDIS_PORT),
+        port: process.env.REDIS_PORT,
     },
 })
 redisClient.on("connect",()=>{
